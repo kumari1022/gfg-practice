172035547a41a8c5723f06b875841154f152ba6d
kumari1022 <practicecoading8@gmail.com>
2025-09-21 10:46:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Does array represent Heap/does-array-represent-heap.java
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.java
@@ -1,17 +1,11 @@
-class Solution {
-
-    public boolean countSub(long arr[], long n) {
-        for (int i = 0; i < n / 2; i++) {
-            int left = 2 * i + 1;
-            int right = 2 * i + 2;
-
-            if (left < n && arr[i] < arr[left]) {
-                return false; // parent smaller than left child
-            }
-            if (right < n && arr[i] < arr[right]) {
-                return false; /
-            }
+class Solution{
+    public boolean countSub(long arr[], long n)
+{
+        for(int i=0;i<=n/2-1;i++){
+            int l=2*i+1,r=2*i+2;
+            if(l<n&&arr[i]<arr[l])return false;
+            if(r<n&&arr[i]<arr[r])return false;
         }
-        return true; 
+        return true;
     }
 }
