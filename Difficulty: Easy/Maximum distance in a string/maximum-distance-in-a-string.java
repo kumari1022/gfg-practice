8377458d1fed893956dcb4bde5ae949998f22393
kumari1022 <practicecoading8@gmail.com>
2025-10-02 16:57:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum distance in a string/maximum-distance-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution{
+    public int maximum_distance(String s){
+        int n=s.length();
+        int[] lastIndex=new int[26];
+        for(int i=0;i<26;i++)lastIndex[i]=-1;
+        int maxDist=-1;
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            int ci=c-'a';
+            if(ci==0)lastIndex[0]=i;
+            else if(lastIndex[ci-1]!=-1){
+                lastIndex[ci]=i;
+                maxDist=Math.max(maxDist,i+1);
+            }
+        }
+        return maxDist==-1?-1:maxDist;
+    }
+}
