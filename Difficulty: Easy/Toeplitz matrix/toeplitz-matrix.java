cb1e7c72a1fcb192680d3a47b05deba18d834334
kumari1022 <practicecoading8@gmail.com>
2025-10-05 20:34:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Toeplitz matrix/toeplitz-matrix.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    boolean isToeplitz(int mat[][])
+    {
+        int n=mat.length,m=mat[0].length;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(mat[i][j]!=mat[i-1][j-1])
+                   return false;
+            }
+        }
+        return true;
+    }
+}
