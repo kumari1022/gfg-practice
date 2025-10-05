2c0ebdd5ab786db460738451d78a15ef954e33fc
kumari1022 <practicecoading8@gmail.com>
2025-10-05 20:02:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Predict the Column/predict-the-column.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    int columnWithMaxZeros(int[][] arr,int N){
+        int maxZeros=0,colIndex=-1;
+        for(int j=0;j<N;j++){
+            int count=0;
+            for(int i=0;i<N;i++)if(arr[i][j]==0)count++;
+            if(count>maxZeros){
+                maxZeros=count;
+                colIndex=j;
+            }
+        }
+        return maxZeros==0?-1:colIndex;
+    }
+}
