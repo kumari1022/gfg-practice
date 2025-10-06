3d6db13f516ab236eec8f502ed5844f50d012ce4
kumari1022 <practicecoading8@gmail.com>
2025-10-06 21:25:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Row Sum in a Matrix/row-sum-in-a-matrix.java
+++ Difficulty: Easy/Row Sum in a Matrix/row-sum-in-a-matrix.java
@@ -1,11 +1,15 @@
-class Solution{
-    public static int[] rowSum(int[][] mat){
+class Solution
+{
+    public static int[] rowSum(int[][] mat)
+    {
         int n=mat.length;
         int m=mat[0].length;
         int[]res=new int[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             int sum=0;
-            for(int j=0;j<m;j++)sum+=mat[i][j];
+            for(int j=0;j<m;j++)
+               sum+=mat[i][j];
             res[i]=sum;
         }
         return res;
