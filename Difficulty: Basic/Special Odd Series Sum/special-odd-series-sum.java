36731731591eb041705155c976e311525c94f7cb
kumari1022 <practicecoading8@gmail.com>
2025-10-20 13:58:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Special Odd Series Sum/special-odd-series-sum.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    static long sumOfTheSeries(long n) {
+        // code here
+        return n * (n + 1) * (2 * n + 1) / 6;
+        
+    }
+}
