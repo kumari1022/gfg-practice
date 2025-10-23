9a71480f5975a1f3aaf540838019b0a201387560
kumari1022 <practicecoading8@gmail.com>
2025-10-23 21:40:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Number in a sorted rotated array/minimum-number-in-a-sorted-rotated-array.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    static int minNumber(int arr[], int low, int high)
+    {
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[high]) low=mid+1;
+            else high=mid;
+        }
+        return arr[low];
+    }
+}
