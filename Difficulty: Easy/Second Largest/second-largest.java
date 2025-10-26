ddba9de20367c7fb0e570f0c6ffa53fdc852a399
kumari1022 <practicecoading8@gmail.com>
2025-10-26 09:20:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    int getSecondLargest(int arr[])
+    {
+        Arrays.sort(arr);
+        int largest=arr[arr.length - 1];
+        for (int i=arr.length-2;i>=0;i--)
+        {
+            if (arr[i]<largest)
+            {
+                return arr[i]; 
+            }
+        }
+        return -1;
+    }
+}
