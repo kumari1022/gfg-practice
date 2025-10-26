8161ee838fc3c8c5bddee91edf3e0cdb4473f139
kumari1022 <practicecoading8@gmail.com>
2025-10-26 10:33:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
+++ Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
@@ -1,6 +1,6 @@
-import java.util.*;
 class Solution{
-    String secFrequent(String arr[],int N){
+    String secFrequent(String arr[],int N)
+    {
         HashMap<String,Integer> map=new HashMap<>();
         for(String s:arr)map.put(s,map.getOrDefault(s,0)+1);
         int first=0,second=0;
