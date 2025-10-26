32c858a82c52e44637c956bc3fbcde827c31f525
kumari1022 <practicecoading8@gmail.com>
2025-10-26 10:27:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.java
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.java
@@ -1,17 +1,20 @@
-class Solution {
-    public static boolean isRotated(String s1, String s2) {
-        if (s1.length() != s2.length()) {
+class Solution
+{
+    public static boolean isRotated(String s1,String s2) 
+    {
+        if (s1.length()!=s2.length())
+        {
             return false;
         }
 
-        if (s1.length() <= 2) {
+        if (s1.length()<=2)
+        {
             return s1.equals(s2);
         }
 
-        int n = s1.length();
-        String clockwise = s1.substring(n - 2) + s1.substring(0, n - 2);
-        String anticlockwise = s1.substring(2) + s1.substring(0, 2);
-
-        return s2.equals(clockwise) || s2.equals(anticlockwise);
+        int n=s1.length();
+        String clockwise=s1.substring(n-2)+s1.substring(0,n-2);
+        String anticlockwise=s1.substring(2)+s1.substring(0,2);
+        return s2.equals(clockwise)||s2.equals(anticlockwise);
     }
 }
