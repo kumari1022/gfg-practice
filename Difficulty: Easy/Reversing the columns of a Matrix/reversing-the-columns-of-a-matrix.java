9f7fa24415e4ca81de0dc8997da337d4e16b90f7
kumari1022 <practicecoading8@gmail.com>
2025-10-28 11:26:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reversing the columns of a Matrix/reversing-the-columns-of-a-matrix.java
+++ Difficulty: Easy/Reversing the columns of a Matrix/reversing-the-columns-of-a-matrix.java
@@ -1,22 +1,21 @@
-class Solution {
-    static void reverseCol(int matrix[][]) {
+class Solution 
+{
+    static void reverseCol(int matrix[][])
+    {
         int n = matrix.length;
-        if (n == 0) return;
-        
+        if (n == 0) 
+        return;
         int m = matrix[0].length;
         if (m <= 1) return;
-
-        // Iterate through each row (n)
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             int left = 0;
             int right = m - 1;
-
-            // Reverse the elements in the current row (O(m) work)
-            while (left < right) {
+            while (left < right)
+            {
                 int temp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
                 matrix[i][right] = temp;
-
                 left++;
                 right--;
             }
