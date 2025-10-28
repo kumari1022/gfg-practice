5fe52cebace237af5d890b0c2171e04614a51515
kumari1022 <practicecoading8@gmail.com>
2025-10-28 11:20:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximum no of 1's row/maximum-no-of-1s-row.java
+++ Difficulty: Easy/Maximum no of 1's row/maximum-no-of-1s-row.java
@@ -1,19 +1,27 @@
-class Sol{
-    public static int maxOnes(int Mat[][], int N, int M){
+class Sol
+{
+    public static int maxOnes(int Mat[][], int N, int M)
+    {
         int maxRow=-1,maxCount=-1;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N;i++)
+        {
             int low=0,high=M-1,firstOne=M;
-            while(low<=high){
+            while(low<=high)
+            {
                 int mid=(low+high)/2;
-                if(Mat[i][mid]==1){
+                if(Mat[i][mid]==1)
+                {
                     firstOne=mid;
                     high=mid-1;
-                }else{
+                }
+                else
+                {
                     low=mid+1;
                 }
             }
             int count=M-firstOne;
-            if(count>maxCount){
+            if(count>maxCount)
+            {
                 maxCount=count;
                 maxRow=i;
             }
