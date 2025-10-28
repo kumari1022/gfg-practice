d705398b6660dd5d666c309f9eabd877323b85a8
kumari1022 <practicecoading8@gmail.com>
2025-10-28 11:20:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Row with Minimum 1s/row-with-minimum-1s.java
+++ Difficulty: Easy/Row with Minimum 1s/row-with-minimum-1s.java
@@ -1,12 +1,15 @@
-class Solution{
-    int minRow(int[][] mat){
+class Solution
+{
+    int minRow(int[][] mat)
+    {
         int n=mat.length,m=mat[0].length;
         int minCount=Integer.MAX_VALUE,minIndex=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             int count=0;
             for(int j=0;j<m;j++)
                 if(mat[i][j]==1)
-                count++;
+                    count++;
             if(count<minCount){
                 minCount=count;
                 minIndex=i;
