07bd94dc7d2198c6c63f7fb66c1901b60f7db8bc
kumari1022 <practicecoading8@gmail.com>
2025-10-28 13:18:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern of Strings/pattern-of-strings.java
+++ Difficulty: Basic/Pattern of Strings/pattern-of-strings.java
@@ -1,12 +1,10 @@
-import java.util.*;
-
-class Solution {
-    ArrayList<String> pattern(String s) {
-        ArrayList<String> result = new ArrayList<>();
-
-        // Loop from full length to 1
-        for (int i = s.length(); i >= 1; i--) {
-            // Add substring from 0 to i
+class Solution 
+{
+    ArrayList<String> pattern(String s) 
+    {
+        ArrayList<String> result=new ArrayList<>();
+        for (int i=s.length();i >= 1;i--)
+        {
             result.add(s.substring(0, i));
         }
 
