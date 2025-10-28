9a8ceaba24576f3db2d8a0a1b9fb0454cc355c5a
kumari1022 <practicecoading8@gmail.com>
2025-10-28 13:23:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Triangle shrinking downwards/triangle-shrinking-downwards.java
+++ Difficulty: Basic/Triangle shrinking downwards/triangle-shrinking-downwards.java
@@ -1,8 +1,10 @@
 class Solution {
-    static String triDownwards(String S){
+    static String triDownwards(String S)
+    {
         StringBuilder sb=new StringBuilder();
         int n=S.length();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             for(int j=0;j<i;j++)
                sb.append('.');
             sb.append(S.substring(i));
