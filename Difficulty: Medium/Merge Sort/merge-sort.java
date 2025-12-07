3c491a15ce5afd446c108517f65686525ff7dcc6
kumari1022 <practicecoading8@gmail.com>
2025-12-07 21:35:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    void mergeSort(int arr[], int l, int r) {
+        // code here
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+    public static void merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++)
+            L[i] = a[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = a[m + 1 + j];
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k++] = L[i++];
+            } else {
+                a[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) {
+            a[k++] = L[i++];
+        }
+        while (j < n2) {
+            a[k++] = R[j++];
+        }
+    }
+}
