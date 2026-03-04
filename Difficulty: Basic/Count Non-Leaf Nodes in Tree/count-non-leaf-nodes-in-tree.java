c5eeb6c8504129272568da0cbd6a2d1b5d963358
kumari1022 <practicecoading8@gmail.com>
2026-03-04 21:40:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Non-Leaf Nodes in Tree/count-non-leaf-nodes-in-tree.java
@@ -0,0 +1,9 @@
+class Solution {
+    int countNonLeafNodes(Node root){
+        if(root==null)
+            return 0;
+        if(root.left==null&&root.right==null)
+            return 0;
+        return 1+countNonLeafNodes(root.left)+countNonLeafNodes(root.right);
+    }
+}
