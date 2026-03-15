a743b3e4d50a522edfff5770db6dab757e27e344
kumari1022 <practicecoading8@gmail.com>
2026-03-15 19:53:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Next Right Node/next-right-node.java
+++ Difficulty: Easy/Next Right Node/next-right-node.java
@@ -1,17 +1,25 @@
-class Solution{
-    Node nextRight(Node root,int key){
+class Solution
+{
+    Node nextRight(Node root,int key)
+    {
         Queue<Node>q=new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
+        while(!q.isEmpty())
+        {
             int size=q.size();
-            for(int i=0;i<size;i++){
+            for(int i=0;i<size;i++)
+            {
                 Node curr=q.poll();
-                if(curr.data==key){
-                    if(i<size-1)return q.peek();
+                if(curr.data==key)
+                {
+                    if(i<size-1)
+                       return q.peek();
                     return new Node(-1);
                 }
-                if(curr.left!=null)q.add(curr.left);
-                if(curr.right!=null)q.add(curr.right);
+                if(curr.left!=null)
+                    q.add(curr.left);
+                if(curr.right!=null)
+                    q.add(curr.right);
             }
         }
         return new Node(-1);
