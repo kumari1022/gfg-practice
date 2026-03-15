a6d24b3546e725ce8f455b9772841b2e4de0ec02
kumari1022 <practicecoading8@gmail.com>
2026-03-15 19:42:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Level of a Node in Binary Tree/level-of-a-node-in-binary-tree.java
+++ Difficulty: Easy/Level of a Node in Binary Tree/level-of-a-node-in-binary-tree.java
@@ -1,20 +1,16 @@
-class Solution {
-
-    int getLevel(Node node, int data) {
-
-        return findLevel(node , data , 1);
+class Solution{
+    int getLevel(Node node,int data){
+        return findLevel(node,data,1);
     }
 
-    int findLevel(Node node , int data , int level) {
+    int findLevel(Node node,int data,int level){
+        if(node==null)return 0;
+        if(node.data==data)return level;
 
-        if (node == null) return 0;
+        int left=findLevel(node.left,data,level+1);
 
-        if (node.data == data) return level;
+        if(left!=0)return left;
 
-        int left = findLevel(node.left , data , level + 1);
-
-        if (left != 0) return left;
-
-        return findLevel(node.right , data , level + 1);
+        return findLevel(node.right,data,level+1);
     }
 }
