9f96d5df033332c10302be3a78974f587cfd7cd2
kumari1022 <practicecoading8@gmail.com>
2026-03-15 19:19:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stack Problem/stack-problem.java
@@ -0,0 +1,16 @@
+class Solution{
+    static Stack<Integer> deleteMid(int n,Stack<Integer> st){
+        int mid=(n/2)+1;
+        solve(st,mid);
+        return st;
+    }
+    static void solve(Stack<Integer> st,int k){
+        if(k==1){
+            st.pop();
+            return;
+        }
+        int x=st.pop();
+        solve(st,k-1);
+        st.push(x);
+    }
+}
